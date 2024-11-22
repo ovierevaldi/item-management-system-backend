ed7f06d0b90faee68747a893ab6484948f140791
Ovie Revaldi <ovierevaldi@gmail.com>
2024-11-22 18:00:38 +0700

    Add CORS

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
-import express from 'express';
+import express, { urlencoded } from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 
 const envFile = process.env.NODE_ENV === 'production' ? '.env' : '.env.development';
 dotenv.config({path: envFile});
 
 const app = express()
 const port = process.env.PORT || 3001;
+
+const corsOptions = {
+  origin: 'http://localhost:4500/'
+};
+app.use(cors(corsOptions));
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
 app.get('/', (req, res) => {
   res.send('Backend!')
